cfa72b0079afdbc0c7a0bb9aa6d59d8180c84590
Balthasar Teuscher <balthasar.teuscher@gmail.com>
2021-06-24 21:22:02 +0200

    Setup pest

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+// use pest::Parser;
+
+// #[derive(Parser)]
+// #[grammar = "../schema/cql.pest"]
+// struct CqlParser;
+
 #[cfg(test)]
 mod tests {
+    use pest::Parser;
+
+    #[derive(Parser)]
+    #[grammar = "../schema/cql.pest"]
+    struct CqlParser;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn parse_pest() {
+        let examples = [
+            "landsat:scene_id = 'LC82030282019133LGN00'",
+            "eo:instrument LIKE 'OLI#' WILDCARD '#'",
+        ];
+        for example in examples {
+            println!("Query: {}", example);
+            let query = CqlParser::parse(Rule::booleanValueExpression, &example)
+                .expect("Failed to parse query");
+            println!("{:?}", query);
+        }
     }
 }
